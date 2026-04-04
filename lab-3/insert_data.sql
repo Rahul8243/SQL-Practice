3843ce6e11062175ac1050567f7da2bb754d6782
Rahul8243 <160226465+Rahul8243@users.noreply.github.com>
2026-04-04 10:44:01 +0530

    insert data

---
+++ lab-3/insert_data.sql
@@ -0,0 +1,36 @@
+INSERT INTO Instructor_details VALUES
+(101,'Ashish','CSE'),
+(102,'Shivam','CSE'),
+(103,'Ravi','ME'),
+(104,'Sujit','CSE'),
+(105,'Alka','CSE'),
+(106,'Suman','CSE'),
+(107,'Mukund','EC'),
+(108,'Rohit','MBA');
+
+
+
+INSERT INTO List_courses
+VALUES
+('CS301', 'DBMS', 4, 3, 101),
+('CS302', 'OS', 3, 3, 102),
+('CS101', 'Physics', 3, 1, 103),
+('CS303', 'Java', 3, 3, 104),
+('CS505', 'Discrete Math', 4, 5, 105),
+('CS701', 'Cloud Computing', 3, 7, 106),
+('EC303', 'DLD', 2, 5, 107),
+('MBA501', 'Economics', 3, 5, 108);
+
+
+INSERT INTO List_student VALUES
+('SCS012','Arjun',3,'CS301',17,101),
+('SCS013','Anamika',3,'CS301',19,101),
+('SCS016','Divya',3,'CS302',20,102),
+('SCS014','Diya',1,'CS101',19,103),
+('SCS015','Abhishek',3,'CS303',17,104),
+('SCS090','Shiva',3,'CS303',16,104),
+('SCS071','Rahul',5,'CS505',21,105),
+('SCS043','Rupam',3,'EC303',22,107),
+('SCS017','Hina',3,'EC303',23,107),
+('SCS022','Alam',5,'MBA501',21,108),
+('SCS056','Satya',7,'CS701',23,106);
