0859236356296aba5b755d689d759b40bdb895a1
Rahul8243 <160226465+Rahul8243@users.noreply.github.com>
2026-04-25 08:13:22 +0530

    added lab-6 insert_data file

---
+++ lab-6/insert_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO Department VALUES
+(1,'IT','Block X'),
+(2,'EEE','Block Y'),
+(3,'Mechanical','Block Z'),
+(4,'Architecture','Block W');
+
+INSERT INTO  Student VALUES
+(201,'ChatGPT',1,'Gurgaon'),
+(202,'Anjali',1,'Faridabad'),
+(203,'Vikas',2,'Delhi'),
+(204,'Sneha',3,'Jaipur'),
+(205,'Karan',NULL,'Chandigarh'),
+(206,'Meena',2,'Noida'),
+(207,'Rahul',2,'bihar');
+
+INSERT INTO Course VALUES
+(301,'Data Science',1,4),
+(302,'Machine Learning',1,3),
+(303,'Power Systems',2,4),
+(304,'Fluid Mechanics',3,3),
+(305,'Building Design',4,3);
+
+INSERT INTO Enrollment VALUES
+(11,201,301,'Sem2',88),
+(12,201,302,'Sem2',82),
+(13,202,301,'Sem2',91),
+(14,203,303,'Sem2',75),
+(15,204,304,'Sem2',69),
+(16,206,303,'Sem2',85);
